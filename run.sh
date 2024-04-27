c1894cdff0f7baf803ae03226cfcc6205651ba99
Javi Martínez <javier.martinez.snt@gmail.com>
2024-04-27 21:07:49 +0200

    Extraccion terminada a 24-04-27

--- run.sh
+++ run.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-Xvfb :99 -ac &
+
+# Iniciar Xvfb y suprimir advertencias
+Xvfb :99 -ac 2>/dev/null &
+
+# Exportar DISPLAY
 export DISPLAY=:99
 
-# Carga las variables de entorno
-set -a  # automáticamente exporta variables
+# Cargar las variables de entorno
+set -a
 source /home/javi/Projects/Yaencontre/.env
-set +a  # deja de exportar automáticamente
+set +a
 
+# Ejecutar el script principal
 python3 /home/javi/Projects/Yaencontre/src/main.py
+
+# Detener Xvfb
 kill %1
 
+# Registrar la hora actual
 time=$(date +"%y-%m-%d")
 
+# Realizar operaciones de git
 git add .
 git commit -m "Extraccion terminada a $time"
-
 git push
