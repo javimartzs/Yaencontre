3b57270dbf8806ad7dd6033a21dc974e8974f273
Javi Martinez <javier.martinez.snt@gmail.com>
2024-05-03 12:11:32 +0200

    Extraccion terminada a 24-05-03

--- run.sh
+++ run.sh
@@ -6,13 +6,17 @@ Xvfb :99 -ac 2>/dev/null &
 # Exportar DISPLAY
 export DISPLAY=:99
 
+# Especificar la ruta completa del intérprete de Python
+PYTHON_INTERPRETER="/usr/bin/python3"  # Cambia esta ruta si es diferente en tu sistema
+
+# Especificar la ruta completa del script .py
+SCRIPT_PATH="/Users/javier.martinez32/Documents/Yaencontre/src/main.py"  # Cambia esta ruta si es diferente
+
 # Cargar las variables de entorno
-set -a
 source /Users/javier.martinez32/Documents/Yaencontre/.env
-set +a
 
 # Ejecutar el script principal
-python3 /Users/javier.martinez32/Documents/Yaencontre/src/main.py
+$PYTHON_INTERPRETER $SCRIPT_PATH
 
 # Detener Xvfb
 kill %1
